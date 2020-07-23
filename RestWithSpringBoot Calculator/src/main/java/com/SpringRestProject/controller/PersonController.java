9c70e44307b5360215e7c34e803dc5bfa1e52916
JaimeGoB <josba93@gmail.com>
2020-07-22 22:15:08 -0500

    Integration with MySQL database already implemented.

--- RestWithSpringBoot Calculator/src/main/java/com/SpringRestProject/controller/PersonController.java
+++ RestWithSpringBoot Calculator/src/main/java/com/SpringRestProject/controller/PersonController.java
@@ -19,6 +19,46 @@ import com.SpringRestProject.services.PersonServices;
 
 /*
 @RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+	
+	@Autowired
+	private PersonServices service;
+	
+	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+	public List<PersonVO> findAll() {
+		return service.findAll();
+	}	
+	
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO findById(@PathVariable("id") Long id) {
+		return service.findById(id);
+	}	
+	
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO create(@RequestBody PersonVO person) {
+		return service.create(person);
+	}
+	
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, 
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO update(@RequestBody PersonVO person) {
+		return service.update(person);
+	}	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		service.delete(id);
+		return ResponseEntity.ok().build();
+	}	
+	
+}
+*/
+
+//Version 1
+/*
+@RestController
 @RequestMapping("/person")
 public class PersonController {
 	
@@ -63,9 +103,10 @@ public class PersonController {
 	}
 }
 */
+//Version 2
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired
---
+++ RestWithSpringBoot Calculator/src/main/resources/db/migration/V2_Populate_Table_Person2.sql
@@ -0,0 +1,3 @@
+USE `rest_with_spring_boot_udemy`;
+
+INSERT INTO `person` VALUES (1,'Brazil','Monic','female','santi'),(3,'Brazil','Monyyyy','female','santiago!!!'),(4,'yuca','yuca','yuca','yuca'),(7,'yuca4','yuca4','yuca4','yuca4');
