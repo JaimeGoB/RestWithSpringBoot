100b373d80ff3f0cda9bfeabb41b2d24c9baaa4e
JaimeGoB <josba93@gmail.com>
2020-07-24 00:13:31 -0500

    New Databases have been added. Everything works fine. Will soon begin with JWT.

--- RestWithSpringBoot Calculator/src/main/java/com/SpringRestProject/config/WebConfig.java
+++ RestWithSpringBoot Calculator/src/main/java/com/SpringRestProject/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,6 +20,13 @@ public class WebConfig implements WebMvcConfigurer{
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
 	}
+	
+	public void addCorseMappings(CorsRegistry registry)
+	{
+		registry.addMapping("/**")
+		.allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}
+	
 	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer ) {
--- RestWithSpringBoot Calculator/src/main/java/com/SpringRestProject/controller/PersonController.java
+++ RestWithSpringBoot Calculator/src/main/java/com/SpringRestProject/controller/PersonController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,6 @@ import com.SpringRestProject.services.PersonServices;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-
 
 @Api(tags = "PersonEndpoint")
 @RestController
